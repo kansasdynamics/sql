f299040046d516e0c58db9bfa6a3f3a4e7e6cdb8
Alex <89443340+kansasdynamics@users.noreply.github.com>
2022-12-10 10:33:55 -0800

    Create joins.sql

---
+++ joins.sql
@@ -0,0 +1,39 @@
+/*****
+This is a template for joining tables on common columns.
+Self, Inner, Left, Right, Full
+*****/
+
+-- Self Join (A single table is joined on itself)
+SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
+FROM Customers A, Customers B
+WHERE A.CustomerID <> B.CustomerID
+AND A.City = B.City
+ORDER BY A.City;
+
+-- Inner Join (Selects records that match in all tables)
+SELECT Orders.OrderID, Customers.CustomerName
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
+
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
+
+-- Left Join (Selects all records from the left table and matching records from the right table)
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+ORDER BY Customers.CustomerName;
+
+-- Right Join (Selects all records from the right table and matching records from the left table)
+SELECT Orders.OrderID, Employees.LastName, Employees.FirstName
+FROM Orders
+RIGHT JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+ORDER BY Orders.OrderID;
+
+-- Full Join (Selects all records when there is a match in either the left or right table)
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Customers.CustomerName;
